1c3bc36945501538d430ae4b62d67dfe1db19403
Julio Santos <julio958214@gmail.com>
2023-05-01 21:19:55 -0300

    Ajusts

--- playwright.config.js
+++ playwright.config.js
@@ -26,20 +26,20 @@ export default defineConfig({
       }
     },
 
-    {
-      name: 'firefox',
-      use: { 
-        ...devices['Desktop Firefox'],
-        viewport: { width: 1920, height: 1080 }
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { 
+    //     ...devices['Desktop Firefox'],
+    //     viewport: { width: 1920, height: 1080 }
+    //   },
+    // },
 
-    {
-      name: 'webkit',
-      use: { 
-        ...devices['Desktop Safari'],
-        viewport: { width: 1920, height: 1080 }
-     },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { 
+    //     ...devices['Desktop Safari'],
+    //     viewport: { width: 1920, height: 1080 }
+    //   },
+    // },
   ],
 });
