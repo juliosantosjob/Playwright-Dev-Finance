7073cd94ef55c1bf55496f613a854cd5823a2a85
Julio Santos <julioc.9730@gmail.com>
2023-11-06 17:32:38 -0300

    Update amounts.spec.js

--- src/tests/amounts.spec.js
+++ src/tests/amounts.spec.js
@@ -2,7 +2,7 @@ import { RegisterExpenses } from '../pages/registExpenses.page';
 import { Amounts } from '../pages/amounts.page';
 import { Helper } from '../support/helper';
 import { test } from '@playwright/test';
-import * from '../support/radomValue';
+import * as radomValue from '../support/radomValue';
 
 test.describe('All tests Amounts', () => {
   const randomAmount = data.getRandomAmount(120, 150);
