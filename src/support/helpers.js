9e49b702c9fa67467881c6cd0ecf80543fad81a9
Julio Santos <julioc.9730@gmail.com>
2024-06-08 18:43:39 -0300

    Atualizar o helpers.js

--- src/support/helpers.js
+++ src/support/helpers.js
@@ -1,12 +1,12 @@
 export function rand(arg1, earg2) {
   if (Array.isArray(arg1)) {
-  const randomIndex = Math.floor(Math.random() * arg1.length);
-  return arg1[randomIndex];
+    const randomIndex = Math.floor(Math.random() * arg1.length);
+    return arg1[randomIndex];
   } else if (typeof arg1 === 'number' && typeof arg2 === 'number') {
-  const randomNumber = Math.floor(Math.random() * (arg1 - arg2 + 1) + arg2);
-  return randomNumber.toString();
+    const randomNumber = Math.floor(Math.random() * (arg1 - arg2 + 1) + arg2);
+    return randomNumber.toString();
   } else {
-  throw new Error("Invalid parameters. Use an array or two numbers.");
+    throw new Error('Invalid parameters. Use an array or two numbers.');
   }
 }
 
