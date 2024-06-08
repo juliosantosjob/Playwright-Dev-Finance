d24383486366101e8eaf70c284536ae597528eaf
Julio Santos <julioc.9730@gmail.com>
2024-06-08 18:20:25 -0300

    Atualizar o amounts.spec.js

--- src/tests/amounts.spec.js
+++ src/tests/amounts.spec.js
@@ -6,24 +6,25 @@ import { test } from '@playwright/test';
 test.describe('All tests for Amounts', () => {
   let registPage, amountPage;
 
+ /* hook for instances of classes */
   test.beforeEach(async ({ page }) => {
     registPage = new RegistExpensesPage(page);
     amountPage = new AmountsPage(page);
   });
-
-  test('For each transaction, add the value in the "total" field', async () => {
+  
+  /* hooks for testing */
+  test.beforeEach(async ({ page }) => {
     await registPage.openApp();
     await registPage.selectNewTransaction();
     await registPage.registerExpense(newExpense);
     await registPage.itRegistered(newExpense);
+  });
+
+  test('For each transaction, add the value in the "total" field', async () => {
     await amountPage.seeTotalAmount(newExpense);
   });
 
   test('For each transaction, add the value in the "Entradas" field', async () => {
-    await registPage.openApp();
-    await registPage.selectNewTransaction();
-    await registPage.registerExpense(newExpense);
-    await registPage.itRegistered(newExpense);
     await amountPage.seeAmountIncome(newExpense);
   });
 });
