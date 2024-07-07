76ef6b7b5c802870023bf16505164168371a6127
Julio Santos <julioc.9730@gmail.com>
2024-07-07 00:02:47 -0300

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     baseURL: 'https://dev-finance.netlify.app',
     screenshot: 'on',
     video: 'off',
-    headless: false,
+    headless: true,
   },
   projects: [
     {
